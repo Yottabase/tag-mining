402ef69ee25cf1ba04bad2ec2467702c1b515441
Christian Vadalà <chr.vadala@gmail.com>
2015-06-09 19:23:33 +0200

    bugfix

--- TagReplacement/src/org/yottabase/tagmining/phraseextractor/PhraseExtractor.java
+++ TagReplacement/src/org/yottabase/tagmining/phraseextractor/PhraseExtractor.java
@@ -1,5 +1,6 @@
 package org.yottabase.tagmining.phraseextractor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.yottabase.tagmining.core.Phrase;
@@ -9,8 +10,9 @@ public class PhraseExtractor implements InterfacePhraseExtractor {
 
 	@Override
 	public List<Phrase> extractPhrases(WebPage htmlPage) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return new ArrayList<Phrase>();
+		
 	}
 
 }
