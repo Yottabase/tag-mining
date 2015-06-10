8d2eb3aeebcda019373df014c73c25cf28772718
Christian Vadalà <chr.vadala@gmail.com>
2015-06-10 09:08:49 +0200

    aggiunge PhraseExtractor semplice

--- TagReplacement/src/org/yottabase/tagmining/phraseextractor/PhraseExtractor.java
+++ TagReplacement/src/org/yottabase/tagmining/phraseextractor/PhraseExtractor.java
@@ -3,16 +3,38 @@ package org.yottabase.tagmining.phraseextractor;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.yottabase.tagmining.core.Phrase;
 import org.yottabase.tagmining.core.WebPage;
 
 public class PhraseExtractor implements InterfacePhraseExtractor {
+	
+	private static final String PUNCTUATION = "\\.?!;";
 
 	@Override
 	public List<Phrase> extractPhrases(WebPage htmlPage) {
 		
-		return new ArrayList<Phrase>();
+		Document document = Jsoup.parse( htmlPage.getPageHtml() );
+		List<Phrase> phrases = new ArrayList<Phrase>();
 		
+		Elements textEl = document.select("p, div");
+		
+		for(Element textNode: textEl){
+			
+			for(String text : textNode.text().split(PUNCTUATION)){
+				
+				if(text.length() == 0) continue;
+				
+				phrases.add(new Phrase(htmlPage.getTrecID(), text));
+				
+			}
+			
+		}
+		
+		return phrases;
 	}
 
 }
