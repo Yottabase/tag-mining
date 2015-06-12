474868ebfc5ac5eeecec1d221093d98cd9536ba1
Christian Vadalà <chr.vadala@gmail.com>
2015-06-12 17:09:38 +0200

    Versione migliorata PhraseExtractor

--- TagReplacement/src/org/yottabase/tagmining/phraseextractor/PhraseExtractor.java
+++ TagReplacement/src/org/yottabase/tagmining/phraseextractor/PhraseExtractor.java
@@ -1,12 +1,23 @@
 package org.yottabase.tagmining.phraseextractor;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+import org.htmlcleaner.CleanerProperties;
+import org.htmlcleaner.DomSerializer;
+import org.htmlcleaner.HtmlCleaner;
+import org.htmlcleaner.TagNode;
+import org.w3c.dom.DOMException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.yottabase.tagmining.core.Phrase;
 import org.yottabase.tagmining.core.WebPage;
 
@@ -16,31 +27,64 @@ public class PhraseExtractor implements InterfacePhraseExtractor {
 	 * Reference: https://html.spec.whatwg.org/multipage/semantics.html
 	 */
 	
-	private static final String PUNCTUATION = "(?<=[.?!;] )";
+	private static final String PUNCTUATION = "(?<=[\\.?!;] )";
+	
+	private static final String XPATH_EXTRACTOR = "//body//text()";
+	
+	private static final String[] SKIPPED_TAGS = {"body", "head", "meta", "figure", "img", "script", "style", "option" };
+	
+	private static final Integer MIN_PHRASE_LENGTH = 5;
 	
 	@Override
 	public List<Phrase> extractPhrases(WebPage htmlPage) {
 		
-		Document document = Jsoup.parse( htmlPage.getPageHtml() );
 		List<Phrase> phrases = new ArrayList<Phrase>();
 		
-		Elements textEl = document.select("div,p,h1,h2,h3,h4,h5,h6");
-		
-		for(Element textNode: textEl){
+		TagNode tagNode = new HtmlCleaner().clean(htmlPage.getPageHtml());
+		try {
 			
-			for(String text : textNode.text().split(PUNCTUATION)){
+			CleanerProperties cleanerProperties = new CleanerProperties();	
+			Document doc = new DomSerializer(cleanerProperties).createDOM(tagNode);
+			
+			XPath xpath = XPathFactory.newInstance().newXPath();
+			NodeList nodes = (NodeList) xpath.evaluate(XPATH_EXTRACTOR, doc, XPathConstants.NODESET);
+			
+			for (int i = 0; i < nodes.getLength(); ++i) {
 				
-				text = text.trim();
+				String tagName = "", parentTagName = "";
 				
-				if(text.length() < 3) continue;
+				Node e = nodes.item(i);
 				
-				phrases.add(new Phrase(htmlPage.getTrecID(), text));
+				tagName = e.getParentNode().getNodeName();
+				if(Arrays.asList(SKIPPED_TAGS).contains(tagName)) continue;
 				
+				try{
+					parentTagName = e.getParentNode().getParentNode().getNodeName();
+					if(tagName.equals("a") && parentTagName.equals("li")) continue;	
+				}catch(NullPointerException e1){
+					//This exception is not relevant
+				}
+				
+				String text = e.getNodeValue();
+				if(text.length() < MIN_PHRASE_LENGTH) continue;
+				text = text.replaceAll("\\r\\n|\\r|\\n", " ");
+				
+				for(String t : text.split(PUNCTUATION)){
+					t = t.trim();
+					if(t.length() < MIN_PHRASE_LENGTH) continue;
+					if(! t.contains(" ")) continue;
+					
+					phrases.add(new Phrase(htmlPage.getTrecID(), t));
+				}
 			}
 			
+		} catch(DOMException e){
+			System.err.println("An invalid or illegal HTML character is specified.");
+			
+		} catch (ParserConfigurationException | XPathExpressionException e) {
+			e.printStackTrace();
 		}
 		
 		return phrases;
 	}
-
 }
