3539cf3b6a26be3b8a541208cab3dc2d5034d3a5
alessandroRastelli <alessandro.rastelli89@gmail.com>
2015-06-17 11:45:48 +0200

    eliminato il rumore derivante dal timeTagMiner

--- TagReplacement/src/org/yottabase/tagmining/tagminer/TimeTagMiner.java
+++ TagReplacement/src/org/yottabase/tagmining/tagminer/TimeTagMiner.java
@@ -5,12 +5,12 @@ public class TimeTagMiner extends RegexTagMiner {
 	private static final String TAG_NAME = "TIME";
 	
 	/*
-	 * 	23:00:00		05:00	10:27PM	 1:31	20:34pm		20:34 pm	12h:12m:12s		22h 12m 12s		12h12m12s
-		10:27PM	 1:31	20:34pm		20:34 pm	05:00: pm  	00:00 PM	12:59:00PM 		1-01-00 PM		1.01.00 PM
+	 * 	23:00:00		05:00	10:27PM	 1:31	20:34pm		20:34 pm	12h:12m:12s		22h 12m 12s		12h12m12s	12h12m12s pm
+		10:27PM	 1:31	20:34pm		20:34 pm	05:00: pm  	00:00 PM	12:59:00PM 		
 		h24	23:00:00	05:00	10:27PM	 1:31	20:34pm		20:34 pm
 	 */
 	
-	private static final String REGEX_TEMPLATE = "(([0-1]?\\d|2?[0-4])h?)(:|-|h|h )(([0-5]?\\d)m?)( |:|-|m)?([0-5]?\\d)s? ?(AM|PM)?\\b";
+	private static final String REGEX_TEMPLATE = "\\b(([0-1]?\\d|2?[0-4])h?)(:|h|h )(([0-5]?\\d)m?)( |:|m)?([0-5]?\\d)s? ?(AM|PM)?\\b";
 	
 	public TimeTagMiner(){
 		
